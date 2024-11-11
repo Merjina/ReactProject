07cc99dc755f37de236f6aa9e2462c6ea8ae5eb9
MdHussain02 <l7creationss@gmail.com>
2024-11-12 00:09:19 +0530

    test

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react'
+import Home from './components/Home'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <div><Home/></div>
+  )
 }
 
-export default App;
+export default App
