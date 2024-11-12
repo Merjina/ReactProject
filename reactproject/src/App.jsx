9f373f73805d0e1e251eca9d1315728072bbe78a
Shilpa8943 <trshilpa14@gmail.com>
2024-11-12 23:01:45 +0530

    inspiration file created

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Test from './components/Test';
 import SearchItems from './components/SearchItems';
+import Inspiration from './components/Inspiration';
 
 
 
@@ -19,7 +20,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/test" element={    <Test/>} />
           <Route path="/search" element={    <SearchItems/>} />
-          
+          <Route path="/about" element={    <Inspiration/>} />
         </Routes>
       </div>
     </Router>
